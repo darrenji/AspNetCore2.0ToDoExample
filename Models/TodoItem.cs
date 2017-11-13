ba881a9ccffa99bff1083c1296943f5be87d0741
darren <764190362@qq.com>
2017-11-13 14:24:35 +0800

    完成第一个controller,创建了数据库

---
+++ Models/TodoItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreTodo.Models
+{
+    public class TodoItem
+    {
+        //这里的主键如果使用Guid，就会报错
+        public int Id { get; set; }
+        public bool IsDone { get; set; }
+        public string Title { get; set; }
+        public DateTimeOffset? DueAt { get; set; }
+    }
+}
---
+++ Services/FakeTodoItemService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Models;
+
+namespace AspNetCoreTodo.Services
+{
+    public class FakeTodoItemService : ITodoItemService
+    {
+        public Task<IEnumerable<TodoItem>> GetIncompleteItemsAsync()
+        {
+            IEnumerable<TodoItem> items = new[]
+            {
+                new TodoItem
+                {
+                    Title="learn asp.net core",
+                    DueAt=DateTimeOffset.Now.AddDays(1)
+                },
+                new TodoItem
+                {
+                    Title ="build awesome apps",
+                    DueAt=DateTimeOffset.Now.AddDays(2)
+                }
+            };
+
+            return Task.FromResult(items);
+        }
+    }
+}
