736a86e257bbfaba02db21c554fc57c0d6705a77
darren <764190362@qq.com>
2017-11-13 20:40:32 +0800

    前端使用jquery, 后端使用entity framework添加数据

--- Services/TodoItemService.cs
+++ Services/TodoItemService.cs
@@ -17,6 +17,21 @@ namespace AspNetCoreTodo.Services
             _context = context;
         }
 
+        public async Task<bool> AddItemAsync(NewTodoItem newItem)
+        {
+            var entity = new TodoItem
+            {
+                IsDone = false,
+                Title = newItem.Title,
+                DueAt = DateTimeOffset.Now.AddDays(3)
+            };
+
+            _context.Items.Add(entity);
+
+            var saveResult = await _context.SaveChangesAsync();
+            return saveResult == 1;
+        }
+
         public async Task<IEnumerable<TodoItem>> GetIncompleteItemsAsync()
         {
             var items = await _context.Items
